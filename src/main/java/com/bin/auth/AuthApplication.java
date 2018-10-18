ad77f30a4082ec656178c0a94b3cb2f124f0b677
aikobin <582748391@qq.com>
2018-10-19 00:24:33 +0800

    ceshi

--- src/main/java/com/bin/auth/AuthApplication.java
+++ src/main/java/com/bin/auth/AuthApplication.java
@@ -2,9 +2,11 @@ package com.bin.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient //开启eureka客户端
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
